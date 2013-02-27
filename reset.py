f86183bae5b784e45ff817b24b1a0d58d761c89e
matt venn <matt@mattvenn.net>
2013-02-27 18:09:28 +0000

    fixed pull up bug, has to be set when defining interrupt, removed showvid

--- reset.py
+++ reset.py
@@ -8,7 +8,7 @@ int_pin = 10
 def do_something(gpio_id, value):
     print "reboot"
     time.sleep(0.5)
-    os.system("shutdown -r +1")
+    os.system("shutdown -r now")
 
 RPIO.add_interrupt_callback(int_pin, do_something, pull_up_down=RPIO.PUD_UP, edge='falling')
 RPIO.wait_for_interrupts()
--- showVid.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#works
-#mplayer -vo fbdev2:/dev/fb0 -framedrop  test.avi
-#test
-mplayer -vo fbdev2:/dev/fb0 -framedrop test.avi
