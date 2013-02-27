6a7093615b52280b84674138e5197bd8bc66e2ea
matt venn <matt@mattvenn.net>
2013-02-27 13:36:17 +0000

    added led output

--- intTest.py
+++ intTest.py
@@ -1,8 +1,22 @@
 import RPIO
+import time
+import os
 
 #gpio 4 is pin 7
+#gpio 3 is pin 5
+led_pin = 11
+int_pin = 9
 def do_something(gpio_id, value):
-    print("New value for GPIO %s: %s" % (gpio_id, value))
+    print "starting vid"
+    RPIO.output(led_pin, True)
+    time.sleep(1)
+    RPIO.output(led_pin, False)
 
-RPIO.add_interrupt_callback(4, do_something, edge='rising')
+    #("New value for GPIO %s: %s" % (gpio_id, value))
+    #os.system("mplayer -vo fbdev2:/dev/fb0 -framedrop test.avi")
+    print "finished"
+
+
+RPIO.setup(led_pin, RPIO.OUT, initial=RPIO.LOW)
+RPIO.add_interrupt_callback(int_pin, do_something, edge='rising')
 RPIO.wait_for_interrupts()
